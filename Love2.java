c148f661921c59040a36d064eb574e84b862525d
dci-student <Terissa>
2023-10-11 16:11:25 +0200

    Love2 Exercise Teacher solution

--- Love2.java
+++ Love2.java
@@ -8,11 +8,19 @@ public class Love2 {
          *            1  2  3   4
          * c1 = "living zebras".charAt(0)
          *
-         * c1= stringArray[0].charAt(indexes[1])
+         * c1= stringArray[0].charAt(indexes[1])                 location [index:0] = 0
          * c2= stringArray[6].charAt(indexes[2])
          * c3= stringArray[7].charAt(indexes[3])
          * c4= stringArray[11].charAt(indexes[4])*/
-        return "";
+
+        String c1, c2, c3, c4;
+        c1 = Character.toString(stringArray[0].charAt(indexes[0]));
+        c2= Character.toString(stringArray[1].charAt(indexes[1]));
+        c3= Character.toString(stringArray[2].charAt(indexes[2]));
+        c4= Character.toString(stringArray[3].charAt(indexes[3]));
+        String loveName = c1+c2+c3+c4;
+
+        return loveName;
     }
 
 }
