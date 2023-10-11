f077bd999a6b4cd2c3c96ec4a3000b63cf2f8643
dci-student <Terissa>
2023-10-11 15:38:08 +0200

    Love2 Exercise

---
+++ Love2.java
@@ -0,0 +1,18 @@
+public class Love2 {
+    /*static if it will be invoked by the class*/  /*returned datatype*/
+    public static String findLoveName(String[] stringArray, int[]indexes){
+        /*
+         * stringArray = ["living zebras", "juicy oranges" ,"do the victory dance", "tusk of an elephant"
+         *                    0                  1                   3                      4
+         * indexes = [0, 6, 7, 11]
+         *            1  2  3   4
+         * c1 = "living zebras".charAt(0)
+         *
+         * c1= stringArray[0].charAt(indexes[1])
+         * c2= stringArray[6].charAt(indexes[2])
+         * c3= stringArray[7].charAt(indexes[3])
+         * c4= stringArray[11].charAt(indexes[4])*/
+        return "";
+    }
+
+}
